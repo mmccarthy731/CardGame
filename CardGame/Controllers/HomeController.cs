93d710ee0ffd3fe9e95d27ec0170d04baf5026c1
Michael R. McCarthy <mmccarthy731@yahoo.com>
2017-11-30 10:33:45 -0500

    allowed for keeping of cards and redrawing, API does not work

--- CardGame/Controllers/HomeController.cs
+++ CardGame/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.IO;
 using Newtonsoft.Json.Linq;
 using System.Net;
+using CardGame.Models;
 
 namespace CardGame.Controllers
 {
@@ -18,8 +19,6 @@ namespace CardGame.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
@@ -32,55 +31,111 @@ namespace CardGame.Controllers
 
         public ActionResult Cards()
         {
+            string deckID = null;
 
-            HttpWebRequest WR = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            if (Session["deckID"] == null)
+            {
+                HttpWebRequest WR = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
 
-            HttpWebResponse Response = (HttpWebResponse)WR.GetResponse();
+                HttpWebResponse Response = (HttpWebResponse)WR.GetResponse();
 
-            StreamReader Reader = new StreamReader(Response.GetResponseStream());
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
 
-            string cardsData = Reader.ReadToEnd();
+                string cardsData = Reader.ReadToEnd();
 
-            JObject JsonData = JObject.Parse(cardsData);
+                JObject JsonData = JObject.Parse(cardsData);
 
-            string deckID = JsonData["deck_id"].ToString();
+                deckID = JsonData["deck_id"].ToString();
 
-            Cookie c = new Cookie("deckID", deckID);
-            Response.Cookies.Add(c);
+                Session.Add("deckID", deckID);
+            }
+            else
+            {
+                deckID = Session["deckID"].ToString();
+            }
+            
+            HttpWebRequest WR2 = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count=5");
 
-            WR = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count=5");
+            HttpWebResponse Response2 = (HttpWebResponse)WR2.GetResponse();
 
-            Response = (HttpWebResponse)WR.GetResponse();
-
-            StreamReader Reader2 = new StreamReader(Response.GetResponseStream());
+            StreamReader Reader2 = new StreamReader(Response2.GetResponseStream());
 
             string DrawnCards = Reader2.ReadToEnd();
 
-            JsonData = JObject.Parse(DrawnCards);
+            ViewBag.Json = DrawnCards;
 
-            ViewBag.Cards = JsonData["cards"];
+            JObject JsonCardData = JObject.Parse(DrawnCards);
+
+            ViewBag.Cards = JsonCardData["cards"];
 
             return View();
         }
-
-        public ActionResult Redraw(int number)
+        //[NonAction]
+        public ActionResult Redraw(string[] code)
         {
-            HttpCookie c = Request.Cookies["deckID"];
+            string deckID = Session["deckID"].ToString();
+            string userHand = "userhand" + deckID;
 
-            string deckID = c.Value.ToString();
+            string keepCards = "";
+            int counter = 5 - code.Length;
+            for (int i = 0; i < code.Length; i++)
+            {
+                    if (i == 0)
+                    {
+                        keepCards += code;
+                    }
+                    else
+                    {
+                        keepCards += "," + code;
+                    }
+            }
 
-            HttpWebRequest WR = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count={number}");
+            if (counter < 5)
+            {
+                HttpWebRequest WR = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/pile/{userHand}/add/?cards={keepCards}");
+            }
 
-            HttpWebResponse Response = (HttpWebResponse)WR.GetResponse();
+            if (counter != 0)
+            {
+                HttpWebRequest WR2 = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count={counter}");
 
-            StreamReader Reader = new StreamReader(Response.GetResponseStream());
+                HttpWebResponse Response = (HttpWebResponse)WR2.GetResponse();
 
-            string drawnCards = Reader.ReadToEnd();
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
 
-            JObject JsonData = JObject.Parse(drawnCards);
+                string drawnCards = Reader.ReadToEnd();
+
+                JObject JsonData = JObject.Parse(drawnCards);
+
+                string newCards = "";
+                for (int i = 0; i < counter; i++)
+                {
+                    if (i < 1)
+                    {
+                        newCards += JsonData["cards"][i]["code"].ToString();
+                    }
+                    else
+                    {
+                        newCards += "," + JsonData["cards"][i]["code"].ToString();
+                    }
+                }
+
+                WR2 = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/pile/{userHand}/add/?cards={newCards}");
+            }
+
+            HttpWebRequest WR3 = WebRequest.CreateHttp($"https://deckofcardsapi.com/api/deck/{deckID}/pile/{userHand}/list");
+
+            HttpWebResponse Response3 = (HttpWebResponse)WR3.GetResponse();
+
+            StreamReader Reader3 = new StreamReader(Response3.GetResponseStream());
+
+            string userCards = Reader3.ReadToEnd();
+
+            JObject JsonData3 = JObject.Parse(userCards);
+
+            ViewBag.Cards = JsonData3["cards"];
 
             return View("Cards");
         }
-
     }
 }
